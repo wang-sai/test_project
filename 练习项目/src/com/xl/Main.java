c88cea2ab80180b0ac5f49a2eaf1bb777f758451
wangsai <dl_ws_20200928@163.com>
2020-09-29 10:43:36 +0800

    “hhh”

--- 练习项目/src/com/xl/Main.java
+++ 练习项目/src/com/xl/Main.java
@@ -6,11 +6,8 @@ public class Main {
          Main main=new Main();
          int x=main.add(20);
         System.out.println(x);
-<<<<<<< HEAD
         System.out.println("aaa");
-=======
         System.out.println("master2");
->>>>>>> 1473309d3685ad01c678fdf4516f9ff6a58c23c2
     }
 
     public int add(int n){
