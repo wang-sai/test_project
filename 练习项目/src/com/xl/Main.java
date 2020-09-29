a1bf3a82a09a44ec1129932f1d2e02604f117e2c
wangsai <dl_ws_20200928@163.com>
2020-09-29 10:21:54 +0800

    “2号”

--- 练习项目/src/com/xl/Main.java
+++ 练习项目/src/com/xl/Main.java
@@ -6,6 +6,7 @@ public class Main {
          Main main=new Main();
          int x=main.add(20);
         System.out.println(x);
+        System.out.println("2号主机1");
     }
 
     public int add(int n){
