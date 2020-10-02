871e077b0b339c7c1ac13f330a2fb4c897fcdf47
wangsai <dl_ws_20200928@163.com>
2020-10-02 11:52:32 +0800

    222

--- 练习项目/src/com/xl/Main.java
+++ 练习项目/src/com/xl/Main.java
@@ -12,11 +12,12 @@ public class Main {
         System.out.println("abc");
         System.out.println("2来了");
         System.out.println("1号主机先改了");
-<<<<<<< HEAD
+
         System.out.println("主修改了");
-=======
+
         System.out.println("开发改了");
->>>>>>> dev
+
+        System.out.println("shehuiren ");
     }
 
     public int add(int n){
